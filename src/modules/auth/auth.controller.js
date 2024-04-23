7120d41fc2441915f4048f5308185f0c50c2aa44
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-04-23 15:15:10 +0330

    change auth contollers

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,39 +1,35 @@
-const autoBind = require("auto-bind");
-const CookieNames = require("../../common/constant/cookieNames.enum");
-const AuthService = require("./auth.service");
+// import autoBind from "auto-bind"
+const CookieNames = require("../../common/constant/cookieNames.enum")
+const messages = require("../messages")
+const AuthService = require("./auth.service")
 
-class AuthController {
-  #service;
-  constructor() {
-    this.#service = AuthService;
-    autoBind(this);
-  }
-  async sendOTP(req, res, next) {
-    try {
-      const { mobile } = req.body;
-      const result = await this.#service.sendOTP(mobile);
-      res.status(result.statusCode ?? 201).send(result);
-    } catch (error) {
-      next(error);
+class AuthController{
+    async sendOTP(req, res, next){
+        try {
+            const {mobile} = req.body
+            const result = await AuthService.sendOTP(mobile)
+            res.status(result.statusCode ?? 201).send(result)
+        } catch (error) {
+            next(error)
+        }
     }
-  }
-  async checkOTP(req, res, next) {
-    try {
-      const { mobile, code } = req.body;
-      const result = await this.#service.checkOTP(mobile, code, res);
-      res.status(result.statusCode ?? 201).send(result);
-    } catch (error) {
-      next(error);
+    async checkOTP(req, res, next){
+        try {
+            const { mobile, code } = req.body
+            const result = await AuthService.checkOTP(mobile, code, res)
+            res.status(result.statusCode ?? 201).send(result)
+        } catch (error) {
+            next(error)
+        }
     }
-  }
-  async logout(req, res, next) {
-    try {
-      return res.clearCookie(CookieNames.accessToken).status(200).send({
-        statusCode: 200,
-      });
-    } catch (error) {
-      next(error);
+    async logout(req, res, next){
+        try {
+            return res.clearCookie(CookieNames.accessToken).status(200).send({
+                statusCode: 200
+            })
+        } catch (error) {
+            next(error)
+        }
     }
-  }
 }
-module.exports = new AuthController();
+module.exports = new AuthController
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -3,10 +3,10 @@ const authController = require("./auth.controller");
 const Authorization = require("../../common/guard/authorization.guard");
 const router = Router();
 
-router.post("/send-otp", authController.sendOTP);
-router.post("/check-otp", authController.checkOTP);
-router.post("/logout", Authorization, authController.logout);
+router.post('/send-otp', authController.sendOTP)
+router.post('/check-otp', authController.checkOTP)
+router.post('/logout', Authorization, authController.logout)
 
 module.exports = {
-  AuthRouters: router,
-};
+    AuthRouters: router
+}
