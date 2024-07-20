bb51f7e100c065c9592f4e1d79f939a42fe1149d
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-07-20 18:51:14 +0330

    update swagger config

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,24 +1,20 @@
 const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerUi = require('swagger-ui-express')
+const swaggerUi = require("swagger-ui-express");
 
 function swaggerConfig(app) {
-    const swaggerDoc = swaggerJSDoc({
-        swaggerDefinition:{
-            openapi: "3.0.1",
-            info: {
-                title: "ponez",
-                version: "1.0.0",
-                description: "ponez swagger docs"
-            }
-        },
-        apis: [
-            process.cwd() + '/src/modules/**/*.swagger.js'
-        ]
-    })
-    const swagger = swaggerUi.setup(
-        swaggerDoc, {}
-    )
-    app.use('/api', swaggerUi.serve, swagger)
+  const swaggerDoc = swaggerJSDoc({
+    swaggerDefinition: {
+      openapi: "3.0.1",
+      info: {
+        title: "ponez",
+        version: "1.0.0",
+        description: "ponez swagger docs",
+      },
+    },
+    apis: [process.cwd() + "/src/modules/**/*.swagger.js"],
+  });
+  const swagger = swaggerUi.setup(swaggerDoc, {});
+  app.use("/api", swaggerUi.serve, swagger);
 }
 
-module.exports = swaggerConfig
+module.exports = swaggerConfig;
